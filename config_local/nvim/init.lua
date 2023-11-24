1615722926e6adf1bfea790d8c64d5fe2dfd30ec
mizuki0629 <mizuki0629@gmail.com>
2023-11-24 14:48:58 +0900

    update

--- config_local/nvim/init.lua
+++ config_local/nvim/init.lua
@@ -8,10 +8,10 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.clipboard = "unnamedplus"
-if vim.fn.executable("python3") then
+if vim.fn.executable("python3") and vim.fn.exists("/.dockerenv") == 0 then
     local venvpath = vim.fn.stdpath("data") .. "/venv"
     if not vim.loop.fs_stat(venvpath) then
-        if vim.fn.has("linux") == 1 and vim.fn.exists("/.dockerenv") == 0 then
+        if vim.fn.has("linux") == 1 then
             vim.fn.system({
                 "/usr/bin/env",
                 "python3",
